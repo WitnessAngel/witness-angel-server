7d545aa25bb3a169534e179caabf7100bd5a7df1
Pascal Chambon <pythoniks@gmail.com>
2022-02-01 11:46:56 +0100

    update CHANGELOG and pyproject.toml version number

--- build_pyinstaller_executable.sh
+++ build_pyinstaller_executable.sh
@@ -1,4 +1,4 @@
 echo "Launching the build of watrustee executable"
-pyinstaller --clean -F --name watrustee src/prod_runner.py
+pyinstaller --clean -F --name waserver src/prod_runner.py
 echo "If success, the generated executable should be in dist/ folder"
 # --distpath ../dist --workpath ../build
