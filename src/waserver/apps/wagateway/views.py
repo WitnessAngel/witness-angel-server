ec155ab1f70b7c8ffa3b39e25c16db15b2e566fb
Pascal Chambon <pythoniks@gmail.com>
2022-07-06 18:02:58 +0200

    Rename wrong "self" parameter to (wsgi) "request" in wagateway/views.py

--- src/waserver/apps/wagateway/views.py
+++ src/waserver/apps/wagateway/views.py
@@ -22,14 +22,14 @@ wagateway_extended_jsonrpc_site = JsonRpcSite(json_encoder=ExtendedDjangoJSONEnc
 @jsonrpc_method("get_public_authenticator", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def get_public_authenticator_view(self, keystore_uid, keystore_secret=None):
+def get_public_authenticator_view(request, keystore_uid, keystore_secret=None):
     return get_public_authenticator(keystore_uid, keystore_secret=keystore_secret)
 
 
 @jsonrpc_method("set_public_authenticator", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def set_public_authenticator_view(self, keystore_uid, keystore_owner, public_keys, keystore_secret,):
+def set_public_authenticator_view(request, keystore_uid, keystore_owner, public_keys, keystore_secret,):
     return set_public_authenticator(keystore_uid=keystore_uid,
                                     keystore_owner=keystore_owner,
                                     public_keys=public_keys,
@@ -39,7 +39,7 @@ def set_public_authenticator_view(self, keystore_uid, keystore_owner, public_key
 @jsonrpc_method("submit_revelation_request", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def submit_decryption_request_view(self, authenticator_keystore_uid, revelation_requestor_uid, revelation_request_description,
+def submit_decryption_request_view(request, authenticator_keystore_uid, revelation_requestor_uid, revelation_request_description,
                                    revelation_response_public_key, revelation_response_keychain_uid, revelation_response_key_algo, symkey_decryption_requests):
     return submit_revelation_request(authenticator_keystore_uid=authenticator_keystore_uid,
                                      revelation_requestor_uid=revelation_requestor_uid,
@@ -53,14 +53,14 @@ def submit_decryption_request_view(self, authenticator_keystore_uid, revelation_
 @jsonrpc_method("list_requestor_revelation_requests", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def list_requestor_revelation_requests_view(self, revelation_requestor_uid):
+def list_requestor_revelation_requests_view(request, revelation_requestor_uid):
     return list_requestor_revelation_requests(revelation_requestor_uid=revelation_requestor_uid)
 
 
 @jsonrpc_method("list_authenticator_revelation_requests", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def list_authenticator_decryption_requests_view(self, authenticator_keystore_uid, authenticator_keystore_secret):
+def list_authenticator_decryption_requests_view(request, authenticator_keystore_uid, authenticator_keystore_secret):
     return list_authenticator_revelation_requests(authenticator_keystore_uid=authenticator_keystore_uid,
                                                   authenticator_keystore_secret=authenticator_keystore_secret)
 
@@ -68,7 +68,7 @@ def list_authenticator_decryption_requests_view(self, authenticator_keystore_uid
 @jsonrpc_method("reject_revelation_request", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def reject_revelation_request_view(self, revelation_request_uid, authenticator_keystore_secret):
+def reject_revelation_request_view(request, revelation_request_uid, authenticator_keystore_secret):
     return reject_revelation_request(revelation_request_uid=revelation_request_uid,
                                      authenticator_keystore_secret=authenticator_keystore_secret)
 
@@ -76,7 +76,7 @@ def reject_revelation_request_view(self, revelation_request_uid, authenticator_k
 @jsonrpc_method("accept_revelation_request", site=wagateway_extended_jsonrpc_site)
 @convert_exceptions_to_jsonrpc_status_slugs
 @validate_input_parameters
-def accept_revelation_request_view(self, revelation_request_uid, symkey_decryption_results, authenticator_keystore_secret):
+def accept_revelation_request_view(request, revelation_request_uid, symkey_decryption_results, authenticator_keystore_secret):
     return accept_revelation_request(revelation_request_uid=revelation_request_uid,
                                      symkey_decryption_results=symkey_decryption_results,
                                      authenticator_keystore_secret=authenticator_keystore_secret)
